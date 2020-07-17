2e081686c47db032bbab3f61de14acad2ba0edf6
zstuder25 <zstuder25@gmail.com>
2020-07-17 15:57:00 -0400

    Section 11: task app rest and mongoose

---
+++ src/db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const connectionUrl = 'mongodb://127.0.0.1:27017/task-manager-api'
+mongoose.connect(connectionUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
